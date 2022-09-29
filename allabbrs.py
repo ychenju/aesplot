9174877b53b47d5644c22ea3ef6fedf183bd0577
ychenju <113119926+ychenju@users.noreply.github.com>
2022-09-29 14:39:08 +0800

    Update allabbrs.py

--- allabbrs.py
+++ allabbrs.py
@@ -20,19 +20,3 @@ from . import tc as aptc
 from . import templates as aptps
 from . import toolkit as aptk
 from . import wrfp as wp
-
-
-# 用法：from aesplot.allabbrs import *
-#
-#   atm         atmospheric science function module
-#   auxf        auxiliary functions (FOR INTERNAL USAGE)
-#   basemap     Basemap supports
-#   figure      figure objects
-#   main        main part (imported with the package)
-#   prep        data preprocessing
-#   settings    internal setting functions
-#   stat        statistics function module
-#   tc          tropical cyclone track processing module
-#   templates   templates
-#   toolkit     useful tool functions for programming (FOR EXTERNAL USAGE)
-#   wrfp        WRFout processing module
