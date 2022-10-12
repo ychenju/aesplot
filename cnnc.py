b5aff7734c25ce787b0617f2ce591cbf98978db5
ychenju <113119926+ychenju@users.noreply.github.com>
2022-10-12 17:28:08 +0800

    Add files via upload

---
+++ cnnc.py
@@ -0,0 +1,60 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import xarray as xr
+from . import wrfp as wp
+from typing import Tuple
+
+class CNnc:
+    
+    def __init__(self, path:str) -> None:
+        self.ncfile = xr.open_dataset(path)
+        self.lat = np.array(self['lat'])
+        self.lon = np.array(self['lon'])
+
+    def __getitem__(self, key:str):
+        return np.array(self.ncfile[key])
+
+    def llbc(self) -> Tuple[np.ndarray]:
+        xlong = np.zeros((self.lat.shape[0], self.lon.shape[0]))
+        xlat = np.zeros((self.lat.shape[0], self.lon.shape[0]))
+        for i, x in enumerate(self.lat):
+            for j, y in enumerate(self.lon):
+                xlat[i][j] = x
+                xlong[i][j] = y
+        return xlat, xlong
+
+class month_mean_temp(CNnc):
+    
+    def __init__(self, path:str) -> None:
+        self.ncfile = xr.open_dataset(path)
+        self.lat = np.array(self['lat'])
+        self.lon = np.array(self['lon'])
+        self.tmps = np.array(self['tmp'])
+
+    def to_wp_frame(self) -> wp.frame:
+        _llbc = self.llbc()
+        _r = wp.voidFrame(*_llbc, time=None)
+        for i in range(len(self.tmps)):
+            _r.set(f'TMP_{i+1}', tmp_conv(self.tmps[i]))
+        return _r
+
+    def to_wp_frames(self) -> Tuple(wp.frame):
+        _llbc = self.llbc()
+        return [wp.voidFrame(*_llbc, time=None).set('TMP', tmp_conv(self.tmps[i])) for i in range(len(self.tmps))]
+
+
+def tmp_conv(tmp:float) -> float:
+    return tmp/10.+273.15
+
+def llbc(data:CNnc):
+    lon = np.array(data['lon'])
+    lat = np.array(data['lat'])
+    xlong = np.zeros((lat.shape[0], lon.shape[0]))
+    xlat = np.zeros((lat.shape[0], lon.shape[0]))
+    for i, x in enumerate(lat):
+        for j, y in enumerate(lon):
+            xlat[i][j] = x
+            xlong[i][j] = y
+    return xlat, xlong
